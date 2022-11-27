0d4fb43fb14df88324bfbe5ee243c9993c575967
Daniel Son <danielson@dhcp-10-250-199-26.harvard.edu>
2022-11-27 18:55:36 -0500

    updating

--- cpsem.py
+++ cpsem.py
@@ -1,9 +1,16 @@
 from mido import MidiFile
 import mido
 
-mid = MidiFile('ValidCounterpoint.mid', clip=True)
+mid = MidiFile('midi_files/ValidCounterpoint.mid', clip=True)
 print(mid)
 
+for i, track in enumerate(mid.tracks):
+    print('Track {}: {}'.format(i, track.name))
+    for msg in track:
+        print(msg)
+
+for msg in mid.play():
+    port.send(msg)
 # for track in mid.tracks:
 #     print(track)
 
@@ -13,14 +20,14 @@ print(mid)
 # for msg in mid.tracks[0]:
 #     print(msg)
 
-print('here', mid.tracks)
-print('\n')
-for i in range(len(mid.tracks[0])):
-    print(mid.tracks[0][i])
+# print('here', mid.tracks)
+# print('\n')
+# for i in range(len(mid.tracks[0])):
+#     print(mid.tracks[0][i])
 
-print('\n')
-for i in range(len(mid.tracks[1])):
-    print(mid.tracks[1][i])
+# print('\n')
+# for i in range(len(mid.tracks[1])):
+#     print(mid.tracks[1][i])
 
 # for msg in mid.tracks[1]:
 #     print(msg)
