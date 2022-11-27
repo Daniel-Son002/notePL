1b9e3be72b82e2e9f6dcfd988a22ad86ffcc0ee3
Daniel Son <danielson@Daniels-MacBook-Pro-2.local>
2022-11-27 17:26:45 -0500

    updating

--- cpsem.py
+++ cpsem.py
@@ -1,10 +1,11 @@
 from mido import MidiFile
+import mido
 
-mid = MidiFile('haydn_sq6.mid', clip=True)
+mid = MidiFile('ValidCounterpoint.mid', clip=True)
 print(mid)
 
-for track in mid.tracks:
-    print(track)
+# for track in mid.tracks:
+#     print(track)
 
 # print('this is track[0]')
 # print(mid.tracks[0])
@@ -12,9 +13,21 @@ for track in mid.tracks:
 # for msg in mid.tracks[0]:
 #     print(msg)
 
-print(len(mid.tracks[1]))
-for i in range(1,20):
+print('here', mid.tracks)
+print('\n')
+for i in range(len(mid.tracks[0])):
+    print(mid.tracks[0][i])
+
+print('\n')
+for i in range(len(mid.tracks[1])):
     print(mid.tracks[1][i])
+
 # for msg in mid.tracks[1]:
 #     print(msg)
 #     break
+# print('2:', mid.tracks)
+# print('3:', mid.tracks[1])
+# print('4:', mid.tracks[1][5])
+
+# msg = mido.Message('note_on', note=60)
+# print(msg)
