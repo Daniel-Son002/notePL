88e753dc43b016b2d1873bdc544ddb137bc2890d
Daniel Son <danielson@college.harvard.edu>
2022-12-15 15:30:57 -0500

    'updating'

--- midi_func.py
+++ midi_func.py
@@ -1,7 +1,7 @@
 from midiutil.MidiFile import MIDIFile
 from midiutil.MidiFile import *
 # import sys
-# import random
+import random
 # from io import parsefile, printfile
 
 keys = {'C': 0, 'G': 7, 'D': 2, 'A': 9, 'E': 4, 'B': 11}
@@ -15,68 +15,59 @@ KEY_NAMES = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
 KEY_NAMES_SHARP = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 KEY_SIG = {'C': 0, 'G': 1, 'D': 2, 'A': 3, 'E': 4, 'B': 5, 'F#': 6, 'C#': 7, 'Ab': 4, 'Eb': 3, 'Bb': 2, 'F': 1}
 
-def applyRules(note, previous):
-    options = [3, 4, 7, 8, 9, 12]  # all intervals allowed in counterpoint
-    if previous is None:
-        options = [7, 12]
-    else:
-        prev_int = previous[1] - previous[0]
-        print(prev_int)
-        print(options)
-        if prev_int == 3 or prev_int == 4:  # no consecutive thirds
-            options.remove(3)
-            options.remove(4)
-        elif prev_int == 8 or prev_int == 9:  # no consecutive sixths
-            options.remove(8)
-            options.remove(9)
-        else:
-            options.remove(prev_int)  # no consecutive 5ths or octaves
+#        C  D  E  F  G  A  B
+C_KEY = [0, 2, 4, 5, 7, 9, 11]
+G_KEY = [0, 2, 4, 6, 7, 9, 11]
+D_KEY = [1, 2, 4, 6, 7, 9, 11]
+A_KEY = [1, 2, 4, 6, 8, 9, 11]
+E_KEY = [1, 3, 4, 6, 8, 9, 11]
+B_KEY = [1, 3, 4, 6, 8, 10, 11]
+Fsharp_KEY = [1, 3, 5, 6, 8, 10, 11]
+Csharp_KEY = [1, 3, 5, 6, 8, 10, 0]
 
-        # if we would approach 5th by parallel motion
-        if (note + 7) - previous[1] > 0 and (note - previous[0]) > 0:
-            if 7 in options:
-                options.remove(7)
-        elif (note + 7) - previous[1] < 0 and (note - previous[0]) < 0:
-            if 7 in options:
-                options.remove(7)
+Ab_KEY = [0, 1, 3, 5, 7, 8, 10]
+Eb_KEY = [0, 2, 3, 5, 7, 8, 10]
+Bb_KEY = [0, 2, 3, 5, 7, 9, 10]
+F_KEY = [0, 2, 4, 5, 7, 9, 10]
 
-        for n in options:
-            if abs((note + n) - previous[1]) > 7:
-                options.remove(n)
+SHARP_KEY = {0: C_KEY, 1: G_KEY, 2: D_KEY, 3: A_KEY, 4: E_KEY, 5: B_KEY, 6: Fsharp_KEY, 7: Csharp_KEY}
+FLAT_KEY = {1: F_KEY, 2: Bb_KEY, 3: Eb_KEY, 4: Ab_KEY}
 
-    return options
+def rand_sign(x):
+    return 1*x if random.random() < 0.5 else -1 * x
 
 
-# def gen(begin):
+def find_key(start):
+    if start % 12 == 0:
+        key = 'C'
+    elif start % 12 == 1:
+        key = 'C#'
+    elif start % 12 == 2:
+        key = 'D'
+    elif start % 12 == 3:
+        key = 'Eb'
+    elif start % 12 == 4:
+        key = 'E'
+    elif start % 12 == 5:
+        key = 'F'
+    elif start % 12 == 6:
+        key = 'F#'
+    elif start % 12 == 7:
+        key = 'G'
+    elif start % 12 == 8:
+        key = 'Ab'
+    elif start % 12 == 9:
+        key = 'A'
+    elif start % 12 == 10:
+        key = 'Bb'
+    elif start % 12 == 11:
+        key = 'B'
+    return key
     
 
 class global_funcs():
-    global lst_to_mid
-    def num(lst, name, start):
-        if start % 12 == 0:
-            key = 'C'
-        elif start % 12 == 1:
-            key = 'C#'
-        elif start % 12 == 2:
-            key = 'D'
-        elif start % 12 == 3:
-            key = 'Eb'
-        elif start % 12 == 4:
-            key = 'E'
-        elif start % 12 == 5:
-            key = 'F'
-        elif start % 12 == 6:
-            key = 'F#'
-        elif start % 12 == 7:
-            key = 'G'
-        elif start % 12 == 8:
-            key = 'Ab'
-        elif start % 12 == 9:
-            key = 'A'
-        elif start % 12 == 10:
-            key = 'Bb'
-        elif start % 12 == 11:
-            key = 'B'
+    global nums
+    def nums(lst, name, start):
 
         MyMIDI = MIDIFile(numTracks=2) 
         upper = 0
@@ -84,22 +75,74 @@ class global_funcs():
         time = 0
         MyMIDI.addTrackName(upper, time, "Counterpoint")
         MyMIDI.addTrackName(lower, time, "Cantus Firmus")
-        MyMIDI.addKeySignature(upper, time, KEY_SIG[key], SHARPS, MINOR)
+        MyMIDI.addKeySignature(upper, time, KEY_SIG[find_key(start)], SHARPS, MAJOR)
+        if 'b' in find_key(start) or find_key(start) == 'F':
+            notes = FLAT_KEY[KEY_SIG[find_key(start)]]
+        else:
+            notes = SHARP_KEY[KEY_SIG[find_key(start)]]
+        print(find_key(start), notes)
         channel = 0
         volume = 100
         duration = 1
-        MyMIDI.addNote(upper, channel, start, time, duration, volume)
-        MyMIDI.addNote(lower, channel, start - 12, time, duration, volume)
+        upper_start = start + 12
+        lower_start = start - 12
+        MyMIDI.addNote(upper, channel, upper_start, time, duration, volume)
+        MyMIDI.addNote(lower, channel, lower_start, time, duration, volume)
         time = 1
+        upper_old = upper_start
+        lower_old = lower_start
+        # old_pitch = curr_pitch = start
         for word in lst:
             for char in word:
-                curr_pitch = ord(char)
+                diff = ord(char) % 12
+                for i in range(diff):
+                    move = rand_sign(1)
+                    if 88 - upper_old > 2 and upper_old - 60 > 2: 
+                        if (upper_old + move) % 12 in notes:
+                            MyMIDI.addNote(upper, channel, upper_old + move, time, duration, volume)
+                            upper_old += move
+                        else:
+                            MyMIDI.addNote(upper, channel, upper_old + 2 * move, time, duration, volume)
+                            upper_old += 2 * move
+                    else:
+                        if 88 - upper_old > upper_old - 60:
+                            move = 1
+                        else:
+                            move = -1
+                        # within upper and lower range
+                        if (upper_old + move) % 12 in notes:
+                            MyMIDI.addNote(upper, channel, upper_old + move, time, duration, volume)
+                            upper_old += move
+                        else:
+                            MyMIDI.addNote(upper, channel, upper_old + 2 * move, time, duration, volume)
+                            upper_old += 2 * move
+                    # C2 = 36 and C4 = 60
+                    if 60 - lower_old > 2 and lower_old - 36 > 2:
+                        if (lower_old + -1 * move) % 12 in notes:
+                            MyMIDI.addNote(lower, channel, lower_old + -1 * move, time, duration, volume)
+                            lower_old += move
+                        else:
+                            MyMIDI.addNote(lower, channel, lower_old + -2 * move, time, duration, volume)
+                            lower_old += 2 * move
+                    else:
+                        if 64 - lower_old > lower_old - 36:
+                            move = 1
+                        else:
+                            move = -1
+                        # within upper and lower range
+                        if (lower_old + move) % 12 in notes:
+                            MyMIDI.addNote(lower, channel, lower_old + move, time, duration, volume)
+                            lower_old += move
+                        else:
+                            MyMIDI.addNote(lower, channel, lower_old + 2 * move, time, duration, volume)
+                            lower_old += 2 * move
+                    time += 1
+                        
 
-                time += 1
-        
         with open("%s.mid" % name, 'wb') as outf:
             MyMIDI.writeFile(outf)
 
+    global lst_to_mid
     def lst_to_mid(lst, name, start):
         if start % 12 == 0:
             key = 'C'
@@ -157,39 +200,6 @@ class global_funcs():
                     MyMIDI.addNote(track, channel, pitch2, time, duration, volume)
                 time += 1
                 old_pitch = curr_pitch
-
-        with open("%s.mid" % name, 'wb') as outf:
-            MyMIDI.writeFile(outf)
-
-    global nums
-    def nums(lst, name, start):
-        MyMIDI = MIDIFile(numTracks=2) 
-        track = 0
-        time = 0
-        MyMIDI.addKeySignature(track, time, 0, 0, 1)
-        MyMIDI.addTrackName(track, time, "Sample Track")
-        MyMIDI.addTempo(track, time, 120)
-        channel = 0
-        volume = 100
-        duration = 1
-        old_pitch = start
-        start_pitch1 = start
-        start_pitch2 = start - 12
-
-        MyMIDI.addNote(track, channel, start_pitch1, time, duration, volume)
-        MyMIDI.addNote(track, channel, start_pitch2, time, duration, volume)
-        time = 1
-
-        for word in lst:
-            for char in word:
-                curr_pitch = ord(char) 
-                if curr_pitch >= 84:
-                    MyMIDI.addNote(track, channel, curr_pitch, time, duration, volume)
-                    MyMIDI.addNote(track, channel, curr_pitch - 4, time, duration, volume)
-                else:
-                    MyMIDI.addNote(track, channel, curr_pitch, time, duration, volume)
-                    MyMIDI.addNote(track, channel, curr_pitch - 7, time, duration, volume)
-                time += 1
 
         with open("%s.mid" % name, 'wb') as outf:
             MyMIDI.writeFile(outf)
--- module1.py
+++ module1.py
@@ -6,7 +6,7 @@ from midi_func import nums
 def concat_str(*args, **kwargs):
     # C
     start_pitch = 60
-    lst_to_mid(args, 'concat_str', start_pitch)
+    nums(args, 'concat_str', start_pitch)
     kwargs_list = ['%s=%s' % (k, kwargs[k]) for k in kwargs]
     print(''.join(args), ','.join(kwargs_list))
 
@@ -14,7 +14,7 @@ def print_str(*args, **kwargs):
     kwargs_list = ['%s=%s' % (k, kwargs[k]) for k in kwargs]
     # D
     start_pitch = 62
-    lst_to_mid(args, 'print_str', start_pitch)
+    nums(args, 'print_str', start_pitch)
     print(' '.join(args), ','.join(kwargs_list))
 
 def add_num(*args, **kwargs):
@@ -28,7 +28,7 @@ def mul_num(*args, **kwargs):
     t = globals()['__builtins__'][kwargs['type']]
     # A
     start_pitch = 69
-    lst_to_mid(args, 'mul_num', start_pitch)
+    nums(args, 'mul_num', start_pitch)
     print(math.prod(map(t, args)))
 
 def div_num(*args, **kwargs):
@@ -39,6 +39,6 @@ def div_num(*args, **kwargs):
         t = globals()['__builtins__'][kwargs['type']]
         # C
         start_pitch = 72
-        lst_to_mid(args, 'div_num', start_pitch)
+        nums(args, 'div_num', start_pitch)
         print(t(args[0]) / t(args[1]))
 
